988454ce75a6a998c2ce09923fb9a11f550ea651
monali-2003 <monalikale08@gmail.com>
2025-12-23 17:08:05 +0530

    this is third commit

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
- return "Hello Monali!\n This is my first flask app."
+ return "Hello Monali!\n This is my first flask app. Now I am adding this line for checking the CI."
 if __name__ == "__main__":
  app.run()
