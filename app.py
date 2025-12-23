bb4617fde387b0739669b252bd58f30413e470ae
monali-2003 <monalikale08@gmail.com>
2025-12-23 16:58:00 +0530

    Change in app

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
- return "Hello Monali!"
+ return "Hello Monali!\n This is my first flask app."
 if __name__ == "__main__":
  app.run()
